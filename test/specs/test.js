e5dbfdabdc9f55534483ca6a8971083223d7167c
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-04-01 13:43:40 -0600

    testing errors in sales tax calculator

--- test/specs/test.js
+++ test/specs/test.js
@@ -10,44 +10,61 @@ describe('Sales Tax Calculator', () => {
         const currentUrl = await browser.getUrl()
         await expect(currentUrl).toBe('https://www.calculator.net/sales-tax-calculator.html')
     })
-    it('Enter a value and calculate', async () => {
-        await SalesTax.calculate({beforeTax:'200'})
-        await expect(SalesTax.result).toHaveText('$213.00')
+    it('Enter no values', async () => {
+        await SalesTax.calculate({})
+    })
+    it('Enter 1 value', async () => {
+        await SalesTax.calculate({afterTax:'300'})
+    })
+    it('Enter 3 values where after tax is less than before tax', async () => {
+        await SalesTax.calculate({beforeTax:'200', taxRate:'10', afterTax:'100'})
+    })
+    it('Enter 2 values where after tax is less than before tax', async () => {
+        await SalesTax.calculate({beforeTax:'200', afterTax:'100'})
+    })
+    it('Enter 3 values where before tax is zero', async () => {
+        await SalesTax.calculate({beforeTax:'0', taxRate:'10', afterTax:'100'})
+    })
+    it('Enter 2 values where before tax is zero', async () => {
+        await SalesTax.calculate({beforeTax:'0', taxRate:'10'})
+    })
+    it('Enter 1 value where before tax is zero', async () => {
+        await SalesTax.calculate({beforeTax:'0'})
     })
 })
 
-describe('Salary Calculator', () => {
-    it('Go to page', async () => {
-        await Salary.navigateToPage()
-        const currentUrl = await browser.getUrl()
-        await expect(currentUrl).toBe('https://www.calculator.net/salary-calculator.html')
-    })
-    it('Enter a value and calculate', async () => {
-        await Salary.calculate({salaryAmount:'18'})
-        await expect(Salary.annualUnadjusted).toHaveText('$37,440')
-    })
-})
+// describe('Salary Calculator', () => {
+//     it('Go to page', async () => {
+//         await Salary.navigateToPage()
+//         const currentUrl = await browser.getUrl()
+//         await expect(currentUrl).toBe('https://www.calculator.net/salary-calculator.html')
+//     })
+//     it('Enter a value and calculate', async () => {
+//         await Salary.calculate({salaryAmount:'18'})
+//         await expect(Salary.annualUnadjusted).toHaveText('$37,440')
+//     })
+// })
 
-describe('Payment Calculator', () => {
-    it('Go to page', async () => {
-        await Payment.navigateToPage()
-        const currentUrl = await browser.getUrl()
-        await expect(currentUrl).toBe('https://www.calculator.net/payment-calculator.html')
-    })
-    it('Enter a value and calculate', async () => {
-        await Payment.calculate({loanAmount:'100'})
-        await expect(Payment.monthlyPayment).toHaveText('Monthly Payment:   $0.84')
-    })
-})
+// describe('Payment Calculator', () => {
+//     it('Go to page', async () => {
+//         await Payment.navigateToPage()
+//         const currentUrl = await browser.getUrl()
+//         await expect(currentUrl).toBe('https://www.calculator.net/payment-calculator.html')
+//     })
+//     it('Enter a value and calculate', async () => {
+//         await Payment.calculate({loanAmount:'100'})
+//         await expect(Payment.monthlyPayment).toHaveText('Monthly Payment:   $0.84')
+//     })
+// })
 
-describe('Compound Interest Calculator', () => {
-    it('Go to page', async () => {
-        await CompoundInterest.navigateToPage()
-        const currentUrl = await browser.getUrl()
-        await expect(currentUrl).toBe('https://www.calculator.net/compound-interest-calculator.html')
-    })
-    it('Enter a value and calculate', async () => {
-        await CompoundInterest.calculate({interestRate:'2'})
-        await expect(CompoundInterest.result).toHaveText('2.01844%')
-    })
-})
+// describe('Compound Interest Calculator', () => {
+//     it('Go to page', async () => {
+//         await CompoundInterest.navigateToPage()
+//         const currentUrl = await browser.getUrl()
+//         await expect(currentUrl).toBe('https://www.calculator.net/compound-interest-calculator.html')
+//     })
+//     it('Enter a value and calculate', async () => {
+//         await CompoundInterest.calculate({interestRate:'2'})
+//         await expect(CompoundInterest.result).toHaveText('2.01844%')
+//     })
+// })
