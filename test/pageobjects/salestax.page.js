d6cadedfc0a0365061b523e596a8e0dd47a6f5b0
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-04-01 13:42:48 -0600

    added error cases and expected error validating to calculate

--- test/pageobjects/salestax.page.js
+++ test/pageobjects/salestax.page.js
@@ -1,4 +1,4 @@
-import { $ } from '@wdio/globals'
+import { $, expect } from '@wdio/globals'
 import Page from './page.js';
 
 class SalesTax extends Page {
@@ -7,17 +7,67 @@ class SalesTax extends Page {
         return $('//input[@id="beforetax"]')
     }
 
+    get inputTaxRate () {
+        return $('//input[@type="text"][@name="taxrate"]')
+    }
+
+    get inputAfterTax () {
+        return $('//input[@type="text"][@name="finalprice"][@id="finalprice"]')
+    }
+
     get buttonCalculate () {
         return $('//input[@type="submit"][@value="Calculate"]');
+    }
+
+    get buttonClear () {
+        return $('//input[@type="button"][@value="Clear"]')
     }
 
     get result () {
         return $('(//*[@class="verybigtext"])[3]//font//b')
     }
 
-    async calculate ({beforeTax}) {
-        await this.inputBeforeTax.setValue(beforeTax)
+    get errorMessage () {
+        return $('//font[@color="red"]')
+    }
+
+    get errorNotValidBeforeTax () {
+        return $('//font[@color="red"][contains(text(),"Please provide a valid before tax price.")]')
+    }
+
+    get errorLessThan2ValuesProvided () {
+        return $('//font[@color="red"][contains(text(),"Please provide at least two values to calculate.")]')
+    }
+
+    get errorAfterTaxCanNotBeSmallerThanBeforeTax () {
+        return $('//font[@color="red"][contains(text(),"After tax price can not be smaller than before tax price.")]')
+    }
+
+    get successMessage () {
+        return $('//h2[@class="h2result"]')
+    }
+
+    async calculate ({beforeTax,taxRate,afterTax}) {
+        let valueCount = 0
+        beforeTax ? (await this.inputBeforeTax.setValue(beforeTax), valueCount++ ): await this.inputBeforeTax.setValue('');
+        taxRate ? (await this.inputTaxRate.setValue(taxRate), valueCount++ ): await this.inputTaxRate.setValue('');
+        afterTax ? (await this.inputAfterTax.setValue(afterTax), valueCount++ ): await this.inputAfterTax.setValue('');
         await this.buttonCalculate.click()
+
+        let expectedErrorMessage = '', expectError = false;
+
+        // These error conditions are in this exact order because the website prioritizes the error message in this exact order.
+        // Each error condition overrides the previous error condition.
+        if (valueCount <=1) {
+            expectedErrorMessage = 'Please provide at least two values to calculate.'; expectError = true;
+        }
+        if ( ((afterTax && beforeTax) && !taxRate) && (Number(afterTax) < Number(beforeTax)) ){
+            expectedErrorMessage = 'After tax price can not be smaller than before tax price.'; expectError = true;
+        }
+        if (beforeTax && ( Number(beforeTax) <= 0 )) {
+            expectedErrorMessage = 'Please provide a valid before tax price.'; expectError = true;
+        }
+        expectError ? await expect(this.errorMessage).toHaveText(expectedErrorMessage) : await expect(this.successMessage).toBeExisting();
     }
 
     navigateToPage () {
