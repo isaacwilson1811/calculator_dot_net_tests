abf631d93f598ed20d0efa5e2af0f4365db667e8
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-04-24 16:03:42 -0600

    removed locate method. use selector getters directly. test spec logic into seperate class properties

--- test/pageobjects/salestax.page.js
+++ test/pageobjects/salestax.page.js
@@ -3,16 +3,17 @@ import BasePage from './basepage.js'
 import GUI from '../requirements/GUI.js'
 
 class SalesTax extends BasePage {
-    openComponentPage () { return super.navigate(this.endpoint) }
     endpoint = 'sales-tax-calculator.html'
-    // Approved Text Content from Design Requirements
+    
+    // Required Assertion Values
     requiredText = {
         title: GUI['Design Requirements'].language.approvedApps.appID34534535.title,
         description: GUI['Design Requirements'].language.approvedApps.appID34534535.description,
         inputLabels: GUI['Design Requirements'].language.approvedApps.appID34534535.inputLabels,
         outputLabels: GUI['Design Requirements'].language.approvedApps.appID34534535.outputLabels
     }
-    // Define getters to return selectors
+
+    // Element Selectors
     get inputPanel () { return $('//div[@class="panel"]') }
     get inputPanelTable () { return $('//div[@class="panel"]/table') }
     get componentHeading () { return $('//h1') }
@@ -28,46 +29,16 @@ class SalesTax extends BasePage {
     get buttonClear () { return $('//input[@type="button"][@value="Clear"]') }
     get buttonSave () { return $('//img[@src="//d26tpo4cm8sb6k.cloudfront.net/img/save.svg"]') }
     get resultHeading () { return $('//h2[@class="h2result"]') }
-    get result () { return $('(//*[@class="verybigtext"])[3]//font//b') }
+    get resultsArray () { return $$('//div/div[@class="verybigtext"]') }
     get resultLine1 () { return $('(//*[@class="verybigtext"])[1]//font') }
     get resultLine2 () { return $('(//*[@class="verybigtext"])[2]//font') }
     get resultLine3 () { return $('(//*[@class="verybigtext"])[3]//font') }
-    get resultsArray () { return $$('//div/div[@class="verybigtext"]') }
     get errorMessage () { return $('//font[@color="red"]') }
     get errorNotValidBeforeTax () { return $('//font[@color="red"][contains(text(),"Please provide a valid before tax price.")]') }
     get errorLessThan2ValuesProvided () { return $('//font[@color="red"][contains(text(),"Please provide at least two values to calculate.")]') }
     get errorAfterTaxCanNotBeSmallerThanBeforeTax () { return $('//font[@color="red"][contains(text(),"After tax price can not be smaller than before tax price.")]') }
-
-    // Abstration layer to map nice string names to getters
-    locate (name) {
-        let getElement = undefined
-        switch(name) {
-            case 'h1 heading': getElement = this.componentHeading; break
-            case 'list of h1 headings': getElement = this.arrayOfComponentHeading; break
-            case 'description': getElement = this.componentDescriptionParagraph; break
-            case 'input container': getElement = this.inputPanel; break
-            case 'input container table': getElement = this.inputPanelTable; break
-            case 'before tax price input': getElement = this.inputBeforeTax; break
-            case 'before tax price label': getElement = this.inputBeforeTaxLabel; break
-            case 'sales tax rate input': getElement = this.inputTaxRate; break
-            case 'sales tax rate label': getElement = this.inputTaxRateLabel; break
-            case 'after tax price input': getElement = this.inputAfterTax; break
-            case 'after tax price label': getElement = this.inputAfterTaxLabel; break
-            case 'calculate button': getElement = this.buttonCalculate; break
-            case 'clear button': getElement = this.buttonClear; break
-            case 'save icon': getElement = this.buttonSave; break
-            case 'result heading': getElement = this.resultHeading; break
-            case 'results as list': getElement = this.resultsArray; break
-            case 'result line 1 value': getElement = this.resultLine1; break
-            case 'result line 2 value': getElement = this.resultLine2; break
-            case 'result line 3 value': getElement = this.resultLine3; break
-            case 'error message': getElement = this.errorMessage; break
-            default: getElement = undefined
-        }
-        return getElement
-    }
     
-    // Component Specific Methods
+    // Main component function
     async calculate ({beforeTax,taxRate,afterTax}) {
         // Check the input params. If it has a value: Input the value, add to count. Does not have a value: Input an empty string, don't count.
         let valueCount = 0
@@ -100,101 +71,92 @@ class SalesTax extends BasePage {
             results.forEach(element => {
                 console.log(JSON.stringify(element))
             })
-            
         }
     }
 
 
-                                            /*
-String named methods to be called from Specs.
-                                            */
+    // Test Spec Logic
     BROWSER = {
-        'Component page is loaded': async () => {
-            await this.openComponentPage()
-        }
+        'Component page is loaded': async () => { await this.openComponentPage(this.endpoint) }
     }
     CALCULATE = {
-        'Valid inputs calculated': async () => {
-            await this.calculate ({ beforeTax: 100, taxRate: 6.5 })
-        }
+        'Valid inputs calculated': async () => { await this.calculate ({ beforeTax: 100, taxRate: 6.5 }) }
     }
     ERROR = {
-        'Invalid inputs produced an error message': async () => {
-            await this.calculate({})
-        }
+        'Invalid inputs produced an error message': async () => { await this.calculate({}) }
     }
     UI = {
         // 1. Heading element used for the component's title.
         'Heading element is the only h1 on the page': async () => {
-            await this.assertArrayLength( await this.locate('list of h1 headings'), { 
+            await this.assertArrayLength( await this.arrayOfComponentHeading, { 
                 expectedLength: 1 
             })
         },
         'Heading text content matches requirement': async () => {
-            await this.assertText( await this.locate('h1 heading'), { 
+            await this.assertText( await this.componentHeading, { 
                 expectedText: this.requiredText.title 
             })
         },
         'Heading text color matches requirement': async () => {
-            await this.assertColor( await this.locate('h1 heading'), {
+            await this.assertColor( await this.componentHeading, {
                 type: 'text', colorFormat: 'hex',
                 expectedColor: this.requiredColors[0]
             })
         },
         // 2. Text element used for the component description.
         'Description element text matches requirement': async () => {
-            await this.assertText ( await this.locate('description'), { 
+            await this.assertText ( await this.componentDescriptionParagraph, { 
                 expectedText: this.requiredText.description 
             })
         },
         // 3. Container element holding value inputs and function buttons.
         'Container has required background color': async () => {
-            await this.assertColor ( await this.locate('input container'), {
+            await this.assertColor ( await this.inputPanel, {
                 type: 'background', colorFormat: 'hex',
                 expectedColor: this.requiredColors[1]
             })
         },
         'Container has required CSS border': async () => {
-            await this.assertCSSBorder ( await this.locate('input container'), {
+            await this.assertCSSBorder ( await this.inputPanel, {
                 expectedColor: this.requiredColors[2],
                 expectedWidth: this.requiredLineProperties[0],
                 expectedStyle: this.requiredLineProperties[1],
             })
         },
         'Container is centered horizontally': async () => {
-            await this.assertAttributeValue ( await this.locate('input container table'), {
+            await this.assertAttributeValue ( await this.inputPanelTable, {
                 attribute: 'align', expectedValue: 'center'
             })
         },
         'Before Tax Price input and label meet requirements': async () => {
-            await this.assertText ( await this.locate ('before tax price label'), {
+            await this.assertText ( await this.inputBeforeTaxLabel, {
                 expectedText: this.requiredText.inputLabels[0]
             })
-            await this.assertBackgroundImage ( await this.locate ('before tax price input'), {
+            await this.assertBackgroundImage ( await this.inputBeforeTax, {
                 expectedImageURL: this.requiredSymbols[1],
                 expectedPosition: '0% 50%'
             })
         },
         'Sales Tax Rate input and label meet requirements': async () => {
-            await this.assertText ( this.locate ('sales tax rate label'), {
+            await this.assertText ( await this.inputTaxRateLabel, {
                 expectedText: this.requiredText.inputLabels[1]
             })
-            await this.assertBackgroundImage ( this.locate ('sales tax rate input'), { 
+            await this.assertBackgroundImage ( await this.inputTaxRate, { 
                 expectedImageURL: this.requiredSymbols[0],
                 expectedPosition: '100% 50%'
             })
         },
         'After Tax Price input and label meet requirements': async () => {
-            await this.assertText ( this.locate ('after tax price label'), {
+            await this.assertText ( this.inputAfterTaxLabel, {
                 expectedText: this.requiredText.inputLabels[2]
             })
-            await this.assertBackgroundImage ( this.locate ('after tax price input'), {
+            await this.assertBackgroundImage ( this.inputAfterTax, {
                 expectedImageURL: this.requiredSymbols[1],
                 expectedPosition: '0% 50%'
             })
         },
         'Calculate button meets requirements': async () => {
-            const button = await this.locate ('calculate button')
+            const button = await this.buttonCalculate
             await this.assertAttributeValue ( button, {
                 attribute: 'value',
                 expectedValue: this.requiredText.inputLabels[3]
@@ -209,7 +171,7 @@ String named methods to be called from Specs.
             })
         },
         'Clear button meets requirments': async () => {
-            const button = await this.locate ('clear button')
+            const button = await this.buttonClear
             await this.assertAttributeValue ( button, {
                 attribute: 'value',
                 expectedValue: this.requiredText.inputLabels[4]
@@ -221,7 +183,7 @@ String named methods to be called from Specs.
         },
         // 4. Enter a valid set of inputs and click Calculate to produce result section
         'Result Heading meets requirements': async () => {
-            const heading = await this.locate ('result heading')
+            const heading = await this.resultHeading
             await this.assertText ( heading, {
                 expectedText: this.requiredText.outputLabels[0]
             })
@@ -235,26 +197,26 @@ String named methods to be called from Specs.
             })
         },
         'Save Icon is aligned to the right': async () => {
-            await this.assertCSSPropertyValue ( await this.locate ('save icon'), {
+            await this.assertCSSPropertyValue ( await this.buttonSave, {
                 property: 'float', expectedValue: 'right'
             })
         },
         'Result text lines meet requirements': async () => {
-            await this.assertArrayLength ( await this.locate ('results as list'), {
+            await this.assertArrayLength ( await this.resultsArray, {
                 expectedLength: 3
             })
-            await this.assertAttributeValue ( await this.locate ('result line 2 value'), {
+            await this.assertAttributeValue ( await this.resultLine2, {
                 attribute: 'color',
                 expectedValue: this.requiredColorsFunctional.success
             })
-            await this.assertAttributeValue ( await this.locate ('result line 3 value'), {
+            await this.assertAttributeValue ( await this.resultLine3, {
                 attribute: 'color',
                 expectedValue: this.requiredColorsFunctional.success
             })
         },
         // 5. Enter an invalid set of inputs and click Calculate to produce an error message
         'Error message is displayed and using required color': async () => {
-            const message = await this.locate('error message')
+            const message = await this.errorMessage
             await this.assertExists(message)
             await this.assertAttributeValue ( message, {
                 attribute: 'color',
