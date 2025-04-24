efd76c4ba4499055437622887c606c9a799cb344
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-04-24 15:57:29 -0600

    wip

--- test/pageobjects/salary.page.js
+++ test/pageobjects/salary.page.js
@@ -1,9 +1,9 @@
-import { $, expect } from '@wdio/globals'
+import { $ } from '@wdio/globals'
 import BasePage from './basepage.js';
 import GUI from '../requirements/GUI.js'
 
 class Salary extends BasePage {
-    openComponentPage () { return super.navigate('salary-calculator.html') }
+    endpoint = 'salary-calculator.html'
 
     // Use properties from the Design Requirements to validate in UI Test
     // Approved Text Content from Design Requirements
@@ -22,17 +22,6 @@ class Salary extends BasePage {
     get annualUnadjusted () { return $('(//table[@class="cinfoT"]//td[contains(text(),"Annual")]/following-sibling::td)[1]')}
     get instructionImg () { return $('//img[@src="//d26tpo4cm8sb6k.cloudfront.net/img/svg/insm.svg"]') }
     // Abstration layer to map nice string names to getters
-    locate (name) {
-        let getElement = undefined
-        switch(name) {
-            case 'heading': getElement = this.componentHeading; break
-            case 'heading as list': getElement = this.arrayOfComponentHeading; break
-            case 'description': getElement = this.componentDescriptionParagraph; break
-            case 'instructions image': getElement = this.instructionImg; break
-            default: getElement = undefined
-        }
-        return getElement
-    }
 
     async calculate ({salaryAmount}) {
         await this.inputSalaryAmount.setValue(salaryAmount)
