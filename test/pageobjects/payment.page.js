892aea08f0b83323dabb1a5d6bc1eca82ee7b8fc
= <=>
2025-04-29 02:40:04 -0600

    wip

--- test/pageobjects/payment.page.js
+++ test/pageobjects/payment.page.js
@@ -2,6 +2,7 @@ import { $ } from '@wdio/globals'
 import BasePage from './basepage.js'
 
 class Payment extends BasePage {
+    endpoint = 'payment-calculator.html'
 
     get inputLoanAmount () {
         return $('//input[@id="cloanamount"]')
@@ -15,13 +16,24 @@ class Payment extends BasePage {
         return $('//h2[@class="h2result"]')
     }
 
-    async calculate ({loanAmount}) {
+    async calculate ({mode, loanTerm, monthlyPay, loanAmount, interestRate}) {
+        if (mode == 'Fixed Term'){
+            await this.buttonFixTerm.click()
+            await this.inputLoanTerm.setValue(loanTerm)
+        } else if (mode == 'Fixed Payments') {
+            await this.buttonFixPay.click()
+            await this.inputmonthlyPay.setValue()
+        }
         await this.inputLoanAmount.setValue(loanAmount)
+        await this.inputInterestRate.setValue(interestRate)
         await this.buttonCalculate.click()
     }
 
-    navigateToPage () {
-        return super.navigate('payment-calculator.html')
+
+    BROWSER = {
+        'Component page is loaded': async () => {
+            await this.openComponentPage(this.endpoint)
+        }
     }
 }
 
