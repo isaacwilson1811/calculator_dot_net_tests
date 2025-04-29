6bd5bf8d6cc901c4c54db965209984ea11051094
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-04-29 12:09:42 -0600

    logic for positive test complete

--- test/pageobjects/payment.page.js
+++ test/pageobjects/payment.page.js
@@ -11,7 +11,10 @@ class Payment extends BasePage {
     get inputLoanAmount () { return $('//input[@id="cloanamount"]') }
     get inputInterestRate () { return $('//input[@id="cinterestrate"]') }
     get buttonCalculate () { return $('//input[@type="submit"][@value="Calculate"]') }
+    get buttonClear () { return $('//input[@type="button"][@value="Clear"]')}
     get resultHeader () { return $('//h2[@class="h2result"]') }
+    get resultText () { return $('(//div[@class="rightresult"]/div)[2]') }
+    get resultTableRows () { return $$('(//table[@class="cinfoT"]/tbody)[1]/tr')}
 
     async calculate ({mode, loanTerm, monthlyPay, loanAmount, interestRate}) {
         if (mode == 'Fixed Term'){
@@ -25,7 +28,21 @@ class Payment extends BasePage {
         await this.inputInterestRate.setValue(interestRate)
         await this.buttonCalculate.click()
     }
+    async verifyResults({mode, header, body, tableData}){
+        await this.assertText( this.resultHeader, { expectedText: header })
+        await this.assertText( this.resultText, { expectedText: body })
+        let dataIndex = 0; let numColumns = 2; let numRows = (mode == 'Fixed Term')? 2 : 3;
+        for (let row = 1; row < numRows+1; row++) {
+            for (let column = 1; column < numColumns+1; column++){
+                const currentCell = $(`(((//table[@class="cinfoT"])[1]/tbody/tr)[${row}]/td)[${column}]`)
+                await this.assertText( currentCell, { expectedText: tableData[dataIndex] })
+                dataIndex++
+            }
+        }
+    }
+    async verifyClear(){
 
+    }
 
     BROWSER = {
         'Component page is loaded': async () => {
@@ -40,8 +57,12 @@ class Payment extends BasePage {
                 loanAmount: '30,000',
                 interestRate: '7'
             })
-            await this.assertText(this.resultHeader,{expectedText:'Monthly Payment:   $594.04'})
-            // Need to assert the rest of the results. this is just a wip
+            await this.verifyResults({
+                mode: 'Fixed Term',
+                header: 'Monthly Payment:   $594.04',
+                body: 'You will need to pay $594.04 every month for 5 years to payoff the debt.',
+                tableData: ['Total of 60 Payments','$35,642.16','Total Interest','$5,642.16']
+            })
         },
         'Positive test sample 2': async () => {
             await this.calculate({
@@ -50,8 +71,28 @@ class Payment extends BasePage {
                 loanAmount: '800',
                 interestRate: '13'
             })
-            await this.assertText(this.resultHeader,{expectedText:'Payoff: 2 years 0.02 months'})
-            // Need to assert the rest of the results. this is just a wip
+            await this.verifyResults({
+                mode: 'Fixed Payments',
+                header: 'Payoff: 2 years 0.02 months',
+                body: 'You will need to pay $38.00 every month for 2 years 0.02 months to payoff the debt.',
+                tableData: ['Time Required to Clear Debt','2.00 years','Total of 24.02 Payments','$912.92','Total Interest','$112.92']
+            })
+        }
+    }
+    CLEAR = {
+        'Inputs empty after clicking Clear Button': async () => {
+            await this.buttonFixTerm.click()
+            await this.inputLoanTerm.setValue('10')
+            await this.buttonFixPay.click()
+            await this.inputMonthlyPay.setValue('10')
+            await this.inputLoanAmount.setValue('10')
+            await this.inputInterestRate.setValue('10')
+            await this.buttonClear.click()
+            await this.assertText(this.inputMonthlyPay, {expectedText: ''})
+            await this.buttonFixTerm.click()
+            await this.assertText(this.inputLoanTerm, {expectedText: ''})
+            await this.assertText(this.inputLoanAmount, {expectedText: ''})
+            await this.assertText(this.inputInterestRate, {expectedText: ''})
         }
     }
 }
