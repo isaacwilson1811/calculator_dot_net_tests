f87615bb927067082ac8a9e29659c3df5b3c3c15
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-05-05 14:24:16 -0600

    break long running it statements into multiple steps

--- test/specs/compound.iobound.js
+++ test/specs/compound.iobound.js
@@ -12,13 +12,61 @@ describe ( 'Navigate to the page for the component being tested.', () => {
 })
 // 1.
 describe ( 'Input min value Interest (0%). Calculate all combinations of both compound selections.', () => {
-    it ( 'All results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
-        await Confirm.CALCULATE[ 'All combinations of min value calculate the same output value of 0' ]()
+    it ( 'All combinations of annually results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All annually combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of semiannually results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All semiannually combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of quarterly results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All quarterly combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of monthly results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All monthly combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of semimonthly results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All semimonthly combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of biweekly results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All biweekly combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of weekly results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All weekly combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of daily results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All daily combinations of min value calculate the same output value of 0' ]()
+    })
+    it ( 'All combinations of continuously results of output interest should be 0.0000000000% or 0% regardless of combination.', async () => {
+        await Confirm.CALCULATE[ 'All continuously combinations of min value calculate the same output value of 0' ]()
     })
 })
 // 2.
 describe ( 'Input max value Interest (99%). Calculate all combinations of both compound selections.', () => {
-    it ( 'Results should match expected results', async () => {
-        await Confirm.CALCULATE[ 'All combinations of max value calculate' ]()
+    it ( 'All combinations of annually should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of annually max value calculate' ]()
+    })
+    it ( 'All combinations of semiannually should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of semiannually max value calculate' ]()
+    })
+    it ( 'All combinations of quarterly should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of quarterly max value calculate' ]()
+    })
+    it ( 'All combinations of monthly should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of monthly max value calculate' ]()
+    })
+    it ( 'All combinations of semimonthly should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of semimonthly max value calculate' ]()
+    })
+    it ( 'All combinations of biweekly should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of biweekly max value calculate' ]()
+    })
+    it ( 'All combinations of weekly should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of weekly max value calculate' ]()
+    })
+    it ( 'All combinations of daily should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of daily max value calculate' ]()
+    })
+    it ( 'All combinations of continuously should match expected results', async () => {
+        await Confirm.CALCULATE[ 'All combinations of continuously max value calculate' ]()
     })
 })
