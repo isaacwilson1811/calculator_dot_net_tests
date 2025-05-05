507471db28608f6670405e9e9b8252efe9430132
Isaac Wilson <isaac.wilson1811@stu.mtec.edu>
2025-05-05 14:26:11 -0600

    nested loop for checking all combinations broken into single loop and multiple calls done with an index option

--- test/pageobjects/compound.page.js
+++ test/pageobjects/compound.page.js
@@ -36,6 +36,23 @@ class CompoundInterest extends BasePage {
         await this.selectOutCompound.selectByAttribute('value', outCompound)
         await this.buttonCalculate.click()
     }
+    async calcCombo ({interestRate, inCompoundOption, expectedResults}) {
+        let calcCount = 0
+        const array = this.selectCompoundValues
+            for (let c2 = 0; c2 < array.length; c2++) {
+                await this.calculate({
+                    interestRate: interestRate,
+                    inCompound: array[inCompoundOption],
+                    outCompound: array[c2]
+                })
+                if (expectedResults.length == 1) {
+                    await this.assertText(this.outPutInterest,{expectedText: expectedResults[0]})
+                } else {
+                    await this.assertText(this.outPutInterest,{expectedText: expectedResults[calcCount]})
+                }
+                calcCount++
+            }
+    }
     async verifyResult({text, output}) {
         await this.assertText(this.resultText,{expectedText: text})
         await this.assertText(this.outPutInterest,{expectedText: output})
@@ -65,44 +82,149 @@ class CompoundInterest extends BasePage {
                 output:'1.12024%'
             })
         },
-        'All combinations of min value calculate the same output value of 0': async () => {
-            const array = this.selectCompoundValues
-            for (let c1 = 0; c1 < array.length; c1++) {
-                for (let c2 = 0; c2 < array.length; c2++) {
-                    await this.calculate({
-                        interestRate: '0',
-                        inCompound: array[c1],
-                        outCompound: array[c2]
-                    })
-                    await this.assertText(this.outPutInterest,{expectedText: '0.0000000000%'})
-                }
-            }
+        'All annually combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 0,
+                expectedResults: ['0.0000000000%']
+            })
         },
-        'All combinations of max value calculate': async () => {
-            const expectedOutPut= [
-                '99.00000%','82.13472%','75.08712%','70.82476%','69.80948%','69.73218%','69.27080%','68.87833%','68.81346%',
-                '123.50250%','99.00000%','89.08077%','83.18156%','81.78797%','81.68206%','81.05040%','80.51385%','80.42524%',
-                '142.19335%','111.25125%','99.00000%','91.79846%','90.10695%','89.97856%','89.21327%','88.56382%','88.45662%',
-                '158.90168%','121.80844%','107.39211%','99.00000%','97.03825%','96.88949%','96.00327%','95.25180%','95.12782%',
-                '163.82767%','124.85546%','109.78855%','101.04188%','99.00000%','98.84521%','97.92320%','97.14154%','97.01259%',
-                '164.22138%','125.09776%','109.97863%','101.20356%','99.15527%','99.00000%','98.07512%','97.29106%','97.16171%',
-                '166.63057%','126.57653%','111.13719%','102.18816%','100.10063%','99.94240%','99.00000%','98.20116%','98.06939%',
-                '168.76326%','127.88002%','112.15624%','103.05297%','100.93068%','100.76984%','99.81191%','99.00000%','98.86607%',
-                '169.12345%','128.09965%','112.32774%','103.19841%','101.07024%','100.90896%','99.94841%','99.13429%','99.00000%'
-            ]
-            let calcCount = 0
-            const array = this.selectCompoundValues
-            for (let c1 = 0; c1 < array.length; c1++) {
-                for (let c2 = 0; c2 < array.length; c2++) {
-                    await this.calculate({
-                        interestRate: '99',
-                        inCompound: array[c1],
-                        outCompound: array[c2]
-                    })
-                    await this.assertText(this.outPutInterest,{expectedText: expectedOutPut[calcCount]})
-                    calcCount++
-                }
-            }
+        'All semiannually combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 1,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All quarterly combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 2,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All monthly combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 3,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All semimonthly combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 4,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All biweekly combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 5,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All weekly combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 6,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All daily combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 7,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All continuously combinations of min value calculate the same output value of 0': async () => {
+            await this.calcCombo({
+                interestRate: '0',
+                inCompoundOption: 8,
+                expectedResults: ['0.0000000000%']
+            })
+        },
+        'All combinations of annually max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 0,
+                expectedResults: [
+                    '99.00000%','82.13472%','75.08712%','70.82476%','69.80948%','69.73218%','69.27080%','68.87833%','68.81346%'
+                ]
+            })
+        },
+        'All combinations of semiannually max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 1,
+                expectedResults: [
+                    '123.50250%','99.00000%','89.08077%','83.18156%','81.78797%','81.68206%','81.05040%','80.51385%','80.42524%'
+                ]
+            })
+        },
+        'All combinations of quarterly max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 2,
+                expectedResults: [
+                    '142.19335%','111.25125%','99.00000%','91.79846%','90.10695%','89.97856%','89.21327%','88.56382%','88.45662%'
+                ]
+            })
+        },
+        'All combinations of monthly max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 3,
+                expectedResults: [
+                    '158.90168%','121.80844%','107.39211%','99.00000%','97.03825%','96.88949%','96.00327%','95.25180%','95.12782%'
+                ]
+            })
+        },
+        'All combinations of semimonthly max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 4,
+                expectedResults: [
+                    '163.82767%','124.85546%','109.78855%','101.04188%','99.00000%','98.84521%','97.92320%','97.14154%','97.01259%'
+                ]
+            })
+        },
+        'All combinations of biweekly max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 5,
+                expectedResults: [
+                    '164.22138%','125.09776%','109.97863%','101.20356%','99.15527%','99.00000%','98.07512%','97.29106%','97.16171%'
+                ]
+            })
+        },
+        'All combinations of weekly max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 6,
+                expectedResults: [
+                    '166.63057%','126.57653%','111.13719%','102.18816%','100.10063%','99.94240%','99.00000%','98.20116%','98.06939%'
+                ]
+            })
+        },
+        'All combinations of daily max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 7,
+                expectedResults: [
+                    '168.76326%','127.88002%','112.15624%','103.05297%','100.93068%','100.76984%','99.81191%','99.00000%','98.86607%'
+                ]
+            })
+        },
+        'All combinations of continuously max value calculate': async () => {
+            await this.calcCombo({
+                interestRate: '99',
+                inCompoundOption: 8,
+                expectedResults: [
+                    '169.12345%','128.09965%','112.32774%','103.19841%','101.07024%','100.90896%','99.94841%','99.13429%','99.00000%'
+                ]
+            })
         }
     }
     CLEAR = {
@@ -251,4 +373,4 @@ class CompoundInterest extends BasePage {
     }
 }
 
-export default new CompoundInterest();
+export default new CompoundInterest()
